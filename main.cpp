e03147b5b5754b18c2e513aeac293b132a3541b3
eldor-galiev <89143463+eldor-galiev@users.noreply.github.com>
2021-12-12 20:38:56 +0000

    added printCountries_with_condition

--- main.cpp
+++ main.cpp
@@ -15,8 +15,8 @@ void printCountries();
 void printCities();
 
 void database_query();
-void printCountries_with_condition();
-void printCities_with_condition();
+void printCountries_with_condition(string field_1,string field_2,string field_3,string condition[3]);
+void printCities_with_condition(string field_1,string field_2,string field_3,string condition[3]);
 
 struct Countries {
   char name[50];
@@ -69,9 +69,9 @@ void console() {
         if (num_prog == 4) {
           printCities();
         }
-        /*if (num_prog == 5) {
+        if (num_prog == 5) {
           database_query();
-        }*/
+        }
         
     }
 }
@@ -179,6 +179,110 @@ void printCities() {
   cout << " " << endl;
 }
 
-void database_query();
-void printCountries_with_condition();
-void printCities_with_condition();
+void database_query() {
+  string field_1, field_2,field_3;
+  string DataBase;
+  string condition[3];
+
+  cout << "SELECT" << endl;
+  cin >> field_1;
+  cin >> field_2;
+  cin >> field_3;
+  cout << "FROM" << endl;
+  cin >> DataBase;
+  if ((DataBase != "Countries") && (DataBase != "countries") && (DataBase != "Cities") && (DataBase != "cities")) {
+    cout << "There is no such database.";
+  }
+  else {
+    cout << "WHERE" << endl;
+    for (int i =0; i < 3; i++) {
+      cin >> condition[i];
+    }
+    
+    cout << "Database Response:" << endl;
+    if ((DataBase == "Countries") || (DataBase == "countries")) {
+      printCountries_with_condition(field_1,field_2,field_3,condition);
+    }
+    /*else {
+      printCities_with_condition(field_1,field_2,field_3,condition);
+    }*/
+  }
+  
+
+  
+}
+
+void printCountries_with_condition(string field_1,string field_2,string field_3,string condition[3]) {
+  FILE *readDateBase = fopen(FILE_Countries,"r");
+  if (readDateBase==NULL){
+      return;
+  }
+  struct Countries Buffer[100];
+  int i = 0;
+  while(fread(&(Buffer[i]), sizeof(Countries), 1, readDateBase)){
+    i++;
+  }
+
+  if (condition[0] == "Name") {
+    if (condition[1] == "=") {
+      if ((field_1 == "Name") && (field_2 == "Capital") && (field_3 == "Area")) {
+        printf("%8s %10s %12s\n", "Name", "Capital", "Area");
+        for (int j = 0; j < i; j++) {
+          if (Buffer[j].name == condition[2]) {
+            printf("%8s %10s %12d\n", Buffer[j].name, Buffer[j].capital, Buffer[j].area);
+          }
+        }
+      }
+      if ((field_1 == "Name") && (field_2 == "Capital") && (field_3 == "-")) {
+        printf("%8s %10s\n", "Name", "Capital");
+        for (int j = 0; j < i; j++) {
+          if (Buffer[j].name == condition[2]) {
+            printf("%8s %10s\n", Buffer[j].name, Buffer[j].capital);
+          }
+        }
+      }
+      if ((field_1 == "Name") && (field_2 == "Area") && (field_3 == "-")) {
+        printf("%8s %12s\n", "Name", "Area");
+        for (int j = 0; j < i; j++) {
+          if (Buffer[j].name == condition[2]) {
+            printf("%8s %12d\n", Buffer[j].name, Buffer[j].area);
+          }
+        }
+      }
+      if ((field_1 == "Name") && (field_2 == "-") && (field_3 == "-")) {
+        printf("%8s\n", "Name");
+        for (int j = 0; j < i; j++) {
+          if (Buffer[j].name == condition[2]) {
+            printf("%8s\n", Buffer[j].name);
+          }
+        }
+      }
+      if ((field_1 == "Capital") && (field_2 == "Area") && (field_3 == "-")) {
+        printf("%8s %12s\n", "Capital", "Area");
+        for (int j = 0; j < i; j++) {
+          if (Buffer[j].name == condition[2]) {
+            printf("%10s %12d\n", Buffer[j].capital, Buffer[j].area);
+          }
+        }
+      }
+      if ((field_1 == "Capital") && (field_2 == "-") && (field_3 == "-")) {
+        printf("%10s\n", "Capital");
+        for (int j = 0; j < i; j++) {
+          if (Buffer[j].name == condition[2]) {
+            printf("%10s\n", Buffer[j].capital);
+          }
+        }
+      }
+      if ((field_1 == "Area") && (field_2 == "-") && (field_3 == "-")) {
+        printf("%12s\n", "Area");
+        for (int j = 0; j < i; j++) {
+          if (Buffer[j].name == condition[2]) {
+            printf("%12d\n", Buffer[j].area);
+          }
+        }
+      }
+    } 
+  }
+  cout << " " << endl;
+}
+void printCities_with_condition(string field_1,string field_2,string field_3,string condition[3]);
