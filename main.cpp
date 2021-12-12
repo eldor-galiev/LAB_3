4052aeec47e48f1cd3921fdd8b5e6acb4ba5cfe6
eldor-galiev <89143463+eldor-galiev@users.noreply.github.com>
2021-12-12 19:32:25 +0000

    added creating

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ void console();
 void createCountry();
 void createCity();
 
-bool isFound();
+bool isFound(char a[50]);
 
 void printCountries();
 void printCities();
@@ -19,8 +19,8 @@ void printCities_with_condition();
 
 struct Countries {
   char name[50];
+  char capital[50];
   int area;
-  char country[50];
 };
 
 struct Cities {
@@ -31,6 +31,8 @@ struct Cities {
 
 const char *FILE_Countries = "DataBase_Countries.dat";
 const char *FILE_Cities = "DataBase_Cities.dat";
+
+int count_Countries = 0;
 
 int main(){
   console();
@@ -43,15 +45,114 @@ void console() {
     while (num_prog != 0) {
         cout << "Select one of the programs and enter it's number:" << endl;
         cout << "0 - Terminate the program." << endl;
-        cout << "1 - Add a new country" << endl;
+        cout << "1 - Add a new country." << endl;
         cout << "2 - Add a new city." << endl;
-        cout << "3 - Search for a queue item." << endl;
-        cout << "4 - Print the queue." << endl;
-        cout << "5 - Change the queue order according to the task." << endl;
+        cout << "3 - Print the <Countries> database." << endl;
+        cout << "4 - Print the <Cities> database." << endl;
+        cout << "5 - Execute a query to the database." << endl;
 
-        cout << ">> " << endl;
+        cout << " " << endl;
+        cout << ">> ";
         cin >> num_prog;
+        cout << " " << endl;
 
+        if (num_prog == 1) {
+          createCountry();
+        }
+        if (num_prog == 2) {
+          createCity();
+        }
+        /*if (num_prog == 3) {
+          printCities();
+        }
+        if (num_prog == 4) {
+          printCities();
+        }
+        if (num_prog == 5) {
+          database_query();
+        }*/
         
     }
 }
+
+void createCountry() {
+  Countries *newCountry = new Countries;
+    cout << "Input arguments:" << endl;
+    cout << "Name of the country >> ";
+    cin >> newCountry->name;
+    cout << " " << endl;
+
+    cout << "Capital of country  >> ";
+    cin >> newCountry -> capital;
+    cout << " " << endl;
+
+    cout << "Country area >> ";
+    cin >> newCountry -> area;
+    cout << " " << endl;
+
+    FILE *Database_Countries = fopen(FILE_Countries, "a");
+    if (Database_Countries == NULL) {
+      cout << "Error" << endl;
+    }
+    else {
+      fwrite(newCountry, sizeof(Countries), 1, Database_Countries);
+    }
+    fclose(Database_Countries);
+    count_Countries++;
+    
+}
+void createCity() {
+  Cities *newCity = new Cities;
+    cout << "Input arguments:" << endl;
+
+    cout << "name of the city >> ";
+    cin >> newCity->name;
+    cout << " " << endl;
+
+    cout << "population >> ";
+    cin >> newCity -> population;
+    cout << " " << endl;
+
+    cout << "country >> ";
+    cin >> newCity -> country;
+    cout << " " << endl;
+
+    if (isFound(newCity -> country)) {
+      FILE *Database_Cities = fopen(FILE_Cities, "a");
+      if (Database_Cities == NULL) {
+        cout << "Error" << endl;
+      }
+      else {
+        fwrite(newCity, sizeof(Cities), 1, Database_Cities);
+      }
+      fclose(Database_Cities);
+    }
+    else {
+      cout << "You can enter the city only of the country that is already in the database." << endl;
+      cout << " " << endl;
+    }
+}
+
+bool isFound(char a[50]) {
+  FILE *readDateBase = fopen(FILE_Countries,"r");
+  if (readDateBase==NULL){
+      return false;
+  }
+  struct Countries Buffer;
+  int i =0;
+  while(fread(&Buffer, sizeof(Countries), 1, readDateBase)){
+      string CNTRY = Buffer.name;
+      if (CNTRY == a) {
+        return true;
+      }
+  }
+  return false;
+  
+}
+
+void printCountries();
+void printCities();
+
+void database_query();
+void printCountries_with_condition();
+void printCities_with_condition();
