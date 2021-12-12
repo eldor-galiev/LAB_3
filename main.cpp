a88665d08c68017d58aa6c7d5b62cd6f84fa84a2
eldor-galiev <89143463+eldor-galiev@users.noreply.github.com>
2021-12-12 18:58:14 +0000

    second_commit

--- main.cpp
+++ main.cpp
@@ -29,7 +29,29 @@ struct Cities {
   char country[50];
 };
 
+const char *FILE_Countries = "DataBase_Countries.dat";
+const char *FILE_Cities = "DataBase_Cities.dat";
 
 int main(){
+  console();
+  return 0;
+}
 
+
+void console() {
+    int num_prog = 10;
+    while (num_prog != 0) {
+        cout << "Select one of the programs and enter it's number:" << endl;
+        cout << "0 - Terminate the program." << endl;
+        cout << "1 - Add a new country" << endl;
+        cout << "2 - Add a new city." << endl;
+        cout << "3 - Search for a queue item." << endl;
+        cout << "4 - Print the queue." << endl;
+        cout << "5 - Change the queue order according to the task." << endl;
+
+        cout << ">> " << endl;
+        cin >> num_prog;
+
+        
+    }
 }
